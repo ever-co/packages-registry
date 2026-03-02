35a28d6fe1e34598ca88365dec3b9d3fe4d33425
Ruslan Konviser <evereq@gmail.com>
2026-03-02 10:07:10 +0100

    fix: simplify entrypoint, remove privilege dropping

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-# Verdaccio runs as UID 10001, GID 65533 in the official image
-VERDACCIO_UID="10001"
-VERDACCIO_GID="65533"
 VERDACCIO_STORAGE="/verdaccio/storage"
 VERDACCIO_CONFIG="/verdaccio/conf/config.yaml"
 LISTEN_PORT="${PORT:-4873}"
@@ -12,8 +9,6 @@ LISTEN_PORT="${PORT:-4873}"
 # Required when using mounted volumes (e.g. Railway, Docker Compose)
 # that default to root ownership.
 mkdir -p "${VERDACCIO_STORAGE}"
-chown -R "${VERDACCIO_UID}:${VERDACCIO_GID}" "${VERDACCIO_STORAGE}"
 
-# Drop privileges and start Verdaccio
-exec su -s /bin/sh "#${VERDACCIO_UID}" -c \
-  "verdaccio --config ${VERDACCIO_CONFIG} --listen http://0.0.0.0:${LISTEN_PORT}"
+# Start Verdaccio
+exec verdaccio --config "${VERDACCIO_CONFIG}" --listen "http://0.0.0.0:${LISTEN_PORT}"
